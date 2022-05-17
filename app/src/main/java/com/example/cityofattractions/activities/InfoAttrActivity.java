68e0a1f917cbdb6c3f227a712dd1b7d3b75a6b5d
GoGI321 <you@example.com>
2022-05-17 17:58:37 +0300

    first commit

---
+++ app/src/main/java/com/example/cityofattractions/activities/InfoAttrActivity.java
@@ -0,0 +1,16 @@
+package com.example.cityofattractions.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cityofattractions.R;
+
+public class InfoAttrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_attr);
+    }
+}
---
+++ app/src/main/java/com/example/cityofattractions/activities/Pay2Activity.java
@@ -0,0 +1,16 @@
+package com.example.cityofattractions.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cityofattractions.R;
+
+public class Pay2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay2);
+    }
+}
